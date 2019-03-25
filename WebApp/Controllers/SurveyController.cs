7aa4ce036d3a810415d42e54d2dc7461b0f1459c
WeeDLY <jorgen.sater@gmail.com>
2019-03-25 12:01:06 +0100

    Ported everything over, but not working.

--- WebApp/Controllers/SurveyController.cs
+++ WebApp/Controllers/SurveyController.cs
@@ -57,5 +57,96 @@ namespace WebApp.Controllers
             ViewBag.result = result;
             return View();
         }
+
+        // GET: Surveys/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // GET: Surveys/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Surveys/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SurveyTitle,DateCreated")] Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Surveys.Add(survey);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(survey);
+        }
+
+        // GET: Surveys/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // POST: Surveys/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SurveyTitle,DateCreated")] Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(survey).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(survey);
+        }
+
+        // GET: Surveys/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // POST: Surveys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Survey survey = db.Surveys.Find(id);
+            db.Surveys.Remove(survey);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
