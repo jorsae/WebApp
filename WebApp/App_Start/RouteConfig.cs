54f6956cb31d42d4ad2ecf60ca55b39d2d05f02e
mariuskarlsen <mariuka@hiof.no>
2019-03-29 13:34:41 +0100

    Added a survey route and made survey the main index page

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace WebApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Survey",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Survey", action = "Index", id = UrlParameter.Optional}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
