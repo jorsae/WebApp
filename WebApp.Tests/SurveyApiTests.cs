eb36843b3dc5171e04cef408f12ba2d45c5a41d7
WeeDLY <jorgen.sater@gmail.com>
2019-04-11 14:04:33 +0200

    Added basic unit testing for SurveyApi

---
+++ WebApp.Tests/SurveyApiTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Tests
+{
+    [TestFixture]
+    public class SurveyApiTests
+    {
+        private SurveyApi surveyApi;
+
+        [SetUp]
+        public void SetUp()
+        {
+            surveyApi = new SurveyApi();
+        }
+
+
+        [Test]
+        public async Task Assert_get_survey_by_id()
+        {
+            Survey survey = await surveyApi.GetSurvey(1);
+            Assert.NotNull(survey);
+        }
+
+        [Test]
+        public async Task Assert_get_all_survey()
+        {
+            List<Survey> surveys = await surveyApi.GetSurveys();
+            Assert.IsNotEmpty(surveys);
+        }
+
+    }
+}
